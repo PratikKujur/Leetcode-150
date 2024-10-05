ff72effd71f7077ee63c1c0d2a331ce1fcb277ac
PratikKujur <pratikkujur121@gmail.com>
2024-10-05 19:24:45 +0530

    Dice-Combinations added

--- DynamicPrograming/Dice-Combinations.py
+++ DynamicPrograming/Dice-Combinations.py
@@ -22,18 +22,30 @@ Input:
 Output:
 4
 """
-if __name__=="__main__":
-    n=int(input())
-    dp=[0]*(n+1)
+import sys
+sys.stdout = open('output.txt', 'w')
+sys.stdin = open('input.txt', 'r')
 
-    dp[0]=1
-    mod=1000000007
+
+    
+ 
+
+n=int(input())
+dp=[0]*(n+1)
+
+dp[0]=1
+mod=1000000007
+
+
+
+for i in range(1,n+1):
+    for j in range(1,7):
+        if(i-j>=0):
+            dp[i]=(dp[i]+dp[i-j])%mod
+
+
+
+print(dp[n])
 
     
 
-    for i in range(1,n+1):
-        for j in range(1,7):
-            if(i-j>=0):
-                dp[i]=(dp[i]+dp[i-j])%mod
-    
-    print(dp[n])
